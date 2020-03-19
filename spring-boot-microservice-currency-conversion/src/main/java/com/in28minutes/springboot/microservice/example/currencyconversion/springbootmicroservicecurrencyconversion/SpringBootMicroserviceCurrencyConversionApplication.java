d0d970e0b70223499b76bc70d915942dcb720b21
gustavoarildo <gustavoarildo@gmail.com>
2020-03-19 10:37:36 -0300

    #13: 2 serviços funcionando

--- spring-boot-microservice-currency-conversion/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
+++ spring-boot-microservice-currency-conversion/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -3,10 +3,11 @@ package com.in28minutes.springboot.microservice.example.currencyconversion.sprin
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("com.in28minutes.springboot.microservice.example.currencyconversion")
+@org.springframework.cloud.openfeign.EnableFeignClients("com.in28minutes.springboot.microservice.example.currencyconversion")
+@EnableDiscoveryClient
 public class SpringBootMicroserviceCurrencyConversionApplication {
 
 	public static void main(String[] args) {
